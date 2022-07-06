2e1f7b259a84f0bae3ab1f5a59caf785856b3765
92U9Jzh8HGRE2HMkoSO2EP1Ks <108777741+92U9Jzh8HGRE2HMkoSO2EP1Ks@users.noreply.github.com>
2022-07-06 00:27:12 -0300

    Update Z8I29URzg2RE7oas0i0I20KR0KjzM.lua

--- Z8I29URzg2RE7oas0i0I20KR0KjzM.lua
+++ Z8I29URzg2RE7oas0i0I20KR0KjzM.lua
@@ -1,18 +1,18 @@
-local Point = game.Players.LocalPlayer.Character.HumanoidRootPart
-local TweenService = game:GetService("TweenService")
-local Virtual = game:GetService("VirtualInputManager")
+local Point = game.Players.LocalPlayer.Character.HumanoidRootPart;
+local TweenService = game:GetService("TweenService");
+local Virtual = game:GetService("VirtualInputManager");
 
 local function Notify(Title,Desc)
-        local CoreGui = game:GetService("CoreGui")
-    local CorePackages = game:GetService("CorePackages")
-    local UIBlox = getrenv().require(CorePackages.UIBlox)
-    UIBlox.init()
-    local StylePalette = getrenv().require(CorePackages.AppTempCommon.LuaApp.Style.StylePalette)
-    local stylePalette = StylePalette.new()
-    stylePalette:updateTheme("dark")
-    stylePalette:updateFont("gotham")
-    local Roact = getrenv().require(CorePackages.Roact)
-    local Images = getrenv().require(CorePackages.Packages._Index.UIBlox.UIBlox.App.ImageSet.Images)
+        local CoreGui = game:GetService("CoreGui");
+    local CorePackages = game:GetService("CorePackages");
+    local UIBlox = getrenv().require(CorePackages.UIBlox);
+    UIBlox.init();
+    local StylePalette = getrenv().require(CorePackages.AppTempCommon.LuaApp.Style.StylePalette);
+    local stylePalette = StylePalette.new();
+    stylePalette:updateTheme("dark");
+    stylePalette:updateFont("gotham");
+    local Roact = getrenv().require(CorePackages.Roact);
+    local Images = getrenv().require(CorePackages.Packages._Index.UIBlox.UIBlox.App.ImageSet.Images);
     local Gui =
         Roact.createElement(
         "ScreenGui",
@@ -35,26 +35,26 @@ local function Notify(Title,Desc)
                 }
             )
         }
-    )
-    Roact.mount(Gui, CoreGui, "InternalStuff")
+    );
+    Roact.mount(Gui, CoreGui, "InternalStuff");
 end
 
-local function Bypass()
-    coroutine.wrap(Notify)("Teleporting...","Please wait. Do not move while the script teleports you to the desired position.")
-    for i = 1, 6 do
-        task.wait()
-        game.Players.LocalPlayer.PlayerGui.PermanentGui.Notifications.Visible = false
+local function Bypass();
+    coroutine.wrap(Notify)("Teleporting...","Please wait. Do not move while the script teleports you to the desired position.");
+    for i = 1, 6 do;
+        task.wait();
+        game.Players.LocalPlayer.PlayerGui.PermanentGui.Notifications.Visible = false;
 
-        Virtual:SendKeyEvent(true, "One", false, game)
-        task.wait(.2)
+        Virtual:SendKeyEvent(true, "One", false, game);
+        task.wait(.2);
     end
     
-    task.wait(2)
-    game:GetService("CoreGui").InternalStuff:Destroy()
+    task.wait(2);
+    game:GetService("CoreGui").InternalStuff:Destroy();
 end
 
 return {
-local TPlayer = function TeleportPlayer(player)
+TPlayer = function TeleportPlayer(player)
     coroutine.wrap(Bypass)();
     local Target = game.Players:FindFirstChild(player).Character.HumanoidRootPart.CFrame;
     task.wait();
@@ -77,7 +77,7 @@ local TPlayer = function TeleportPlayer(player)
     Point.Anchored = false;
 end,
 
-local TPosition = function TeleportPos(pos)
+TPosition = function TeleportPos(pos)
     coroutine.wrap(Bypass)();
     local Target = pos;
     task.wait();
